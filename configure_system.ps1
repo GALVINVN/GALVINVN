0a31b6701facbc4433b33eec63899b57f364c7c0
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2026-08-14 16:46:26 +0700

    Update configure_system.ps1

--- configure_system.ps1
+++ configure_system.ps1
@@ -296,5 +296,4 @@ Stop-Service -Name "WSearch" -Force
 Remove-Item -Path "C:\ProgramData\Microsoft\Search\Data\*" -Recurse -Force
 Set-Service -Name "WSearch" -StartupType Disabled
 gpupdate /force
-Restart-Computer -Force
 exit
